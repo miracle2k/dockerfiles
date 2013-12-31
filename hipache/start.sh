10b662744d3e868c066ca5b3a0e464096935d6f3
root <michael@elsdoerfer.com>
2013-12-31 22:43:24 +0100

    hipache: create necessary directories for keys.

--- hipache/start.sh
+++ hipache/start.sh
@@ -7,12 +7,14 @@ echo $HIPACHE_CONFIG | cat /usr/local/lib/node_modules/hipache/config/config.jso
 if [ -n "$SSL_KEY" ]; then
     file=$(cat $config | json server.https.key)
     echo "Writing SSL_KEY to $file"
+    mkdir -p "$(dirname $file)"
     echo "$SSL_KEY" > file
 fi
 
 if [ -n "$SSL_CERT" ]; then
     file=$(cat $config | json server.https.cert)
     echo "Writing SSL_CERT to $file"
+    mkdir -p "$(dirname $file)"
     echo "$SSL_CERT" > file
 fi
 
