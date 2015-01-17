832fe9046d08d8d599c57b7e043d800113c1fb43
Michael Elsdörfer <michael@elsdoerfer.com>
2015-01-18 00:13:41 +0100

    Port back config file customizations to v7 config.

--- sentry/sentry.conf.py
+++ sentry/sentry.conf.py
@@ -7,27 +7,28 @@ import os.path
 
 CONF_ROOT = os.path.dirname(__file__)
 
+
+database_name = os.environ.get('SENTRY_NAME', 'sentry')
+database_user = os.environ.get('SENTRY_USER', 'sentry')
+database_password = os.environ.get('SENTRY_PASS', 'sentry')
+database_host = os.environ.get('SENTRY_HOST', '127.0.0.1')
+database_port = os.environ.get('SENTRY_PORT', '')
+
+
 DATABASES = {
     'default': {
-        # You can swap out the engine for MySQL easily by changing this value
-        # to ``django.db.backends.mysql`` or to PostgreSQL with
-        # ``django.db.backends.postgresql_psycopg2``
-
-        # If you change this, you'll also need to install the appropriate python
-        # package: psycopg2 (Postgres) or mysql-python
-        'ENGINE': 'django.db.backends.sqlite3',
-
-        'NAME': os.path.join(CONF_ROOT, 'sentry.db'),
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': os.environ.get('SENTRY_ENGINE', 'django.db.backends.sqlite3'),  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': database_name,                      # Or path to database file if using sqlite3.
+        'USER': database_user,                      # Not used with sqlite3.
+        'PASSWORD': database_password,                  # Not used with sqlite3.
+        'HOST': database_host,                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': database_port,                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 # You should not change this setting after your database has been created
 # unless you have altered all schemas first
-SENTRY_USE_BIG_INTS = True
+SENTRY_USE_BIG_INTS = False if os.environ.get('NO_BIG_INTS', False) else True
 
 # If you're expecting any kind of real traffic on Sentry, we highly recommend
 # configuring the CACHES and Redis settings
@@ -131,7 +132,7 @@ SENTRY_FILESTORE_OPTIONS = {
 ################
 
 # You MUST configure the absolute URI root for Sentry:
-SENTRY_URL_PREFIX = 'http://sentry.example.com'  # No trailing slash!
+SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', '')
 
 # If you're using a reverse proxy, you should enable the X-Forwarded-Proto
 # and X-Forwarded-Host headers, and uncomment the following settings
@@ -153,45 +154,51 @@ SENTRY_WEB_OPTIONS = {
 # For more information check Django's documentation:
 #  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' if os.environ.get('SENTRY_EMAIL_ENABLED') else 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_HOST_USER = ''
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
+EMAIL_HOST = os.environ.get('SENTRY_EMAIL_HOST', 'localhost')
+EMAIL_HOST_PASSWORD = os.environ.get('SENTRY_EMAIL_PASSWORD', '')
+EMAIL_HOST_USER = os.environ.get('SENTRY_EMAIL_USER','')
+EMAIL_PORT = os.environ.get('SENTRY_EMAIL_PORT', 25)
+EMAIL_USE_TLS = os.environ.get('SENTRY_EMAIL_USE_TLS', False)
 
 # The email address to send on behalf of
-SERVER_EMAIL = 'root@localhost'
+
+SERVER_EMAIL = os.environ.get('SENTRY_EMAIL_FROM', 'root@localhost')
 
 ###########
 ## etc. ##
 ###########
 
+## Set this to false to require authentication
+SENTRY_PUBLIC = False
+SENTRY_ALLOW_REGISTRATION = False
+
 # If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
 # Changing this value will result in all current sessions being invalidated
-SECRET_KEY = 'GbiDHT4et09gBKnjAl6kr8WO0UmvBpIKeRQKMXMnY0ZzaABvKcCixw=='
+SENTRY_KEY = os.environ.get('SENTRY_KEY', base64.urlsafe_b64encode(os.urandom(50)))
+
 
 # http://twitter.com/apps/new
 # It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 
 # http://developers.facebook.com/setup/
-FACEBOOK_APP_ID = ''
-FACEBOOK_API_SECRET = ''
+FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
+FACEBOOK_API_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
 
 # http://code.google.com/apis/accounts/docs/OAuth2.html#Registering
-GOOGLE_OAUTH2_CLIENT_ID = ''
-GOOGLE_OAUTH2_CLIENT_SECRET = ''
+GOOGLE_OAUTH2_CLIENT_ID = os.environ.get('GOOGLE_OAUTH2_CLIENT_ID')
+GOOGLE_OAUTH2_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH2_CLIENT_SECRET')
 
 # https://github.com/settings/applications/new
-GITHUB_APP_ID = ''
-GITHUB_API_SECRET = ''
+GITHUB_APP_ID = os.environ.get('GITHUB_APP_ID')
+GITHUB_API_SECRET = os.environ.get('GITHUB_APP_SECRET')
 
 # https://trello.com/1/appKey/generate
-TRELLO_API_KEY = ''
-TRELLO_API_SECRET = ''
+TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
+TRELLO_API_SECRET = os.environ.get('TRELLO_API_SECRET')
 
 # https://confluence.atlassian.com/display/BITBUCKET/OAuth+Consumers
 BITBUCKET_CONSUMER_KEY = ''
